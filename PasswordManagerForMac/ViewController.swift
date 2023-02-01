452722d0bbb59a52bb2976828d1f5cd2c99f39d3
RyoKurachi <ryokurachiryo@gmail.com>
2023-02-01 23:52:35 +0900

    Initial Commit

---
+++ PasswordManagerForMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PasswordManagerForMac
+//
+//  Created by 倉知諒 on 2023/02/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
